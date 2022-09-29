fc34f45b8e5c5fff9faf8c3b409e8316d8514c22
Zapata <zapati99@gmail.com>
2022-09-29 12:47:49 +0200

    Hola

--- src/InterfazCalamar.java
+++ src/InterfazCalamar.java
@@ -30,7 +30,7 @@ public class InterfazCalamar extends JFrame {
 	 */
 	public InterfazCalamar() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 450, 301);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
